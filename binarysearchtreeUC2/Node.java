8fae8491cbac088e5c54a225949af5a8d83baeea
SumeetCodeJC <code.sumeet9@gmail.com>
2022-03-07 19:44:08 +0530

    Added : Day 15 Prac Probs

---
+++ binarysearchtreeUC2/Node.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.binarysearchtreeUC2;
+
+public class Node {
+	Node left;
+	Node right;
+		int data;
+
+}
---
+++ binarysearchtreeUC3/Node.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.binarysearchtreeUC3;
+
+public class Node {
+	Node left;
+	Node right;
+		int data;
+
+}
